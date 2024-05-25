2e4bcff8105ba8ed67828266bcec376797c513fa
shubhamkambleX <196shubham@gmail.com>
2024-05-25 14:12:10 +0530

    updated the code

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name='mcq_generator',
+    version='0.0.1',
+    author='shubham Kamble',
+    author_email='196shubham62@gmail.com',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+    
+)
